33d7e2c8730840b54bba8293c224a1b0d5fbf0df
yaosiyuan <yaosiyuanmail@163.com>
2020-03-04 16:05:57 +0800

    update:(all):提交项目

---
+++ README.md
@@ -0,0 +1,45 @@
+# 项目说明
+浙江中亿 springboot 简单注册登录。
+
+项目地址：
+
+# 需求分析
+
+......
+
+# 项目构架
+因为任何要求，所以采用最简单的html。完成简单的增删查改。
+- spring.boot 1.5
+- mysql5.6
+- jdk 1.8
+- layuimini
+
+# 项目部署
+- idea 导入项目
+- mysql导入login.sql
+- ap.yml更改数据库密码
+- 运行Application
+
+
+# 项目演示
+运行成功后 打开http://localhost:8080/regist.html
+
+## 注册
+
+![image-20200304160122389](README/image-20200304160122389.png)
+
+
+
+输入测试数据
+
+显示注册成功
+
+![image-20200304160144642](README/image-20200304160144642.png)
+
+## 登录
+
+http://localhost:8080/login.html
+
+![image-20200304155957989](README/image-20200304155957989.png)
+
+![image-20200304160010035](README/image-20200304160010035.png)
---
+++ login.sql
@@ -0,0 +1,37 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50527
+ Source Host           : localhost:3306
+ Source Schema         : login
+
+ Target Server Type    : MySQL
+ Target Server Version : 50527
+ File Encoding         : 65001
+
+ Date: 04/03/2020 16:02:29
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint(32) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, '123', '123');
+INSERT INTO `user` VALUES (2, '111', '111');
+
+SET FOREIGN_KEY_CHECKS = 1;
